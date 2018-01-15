2dea89dcc720d0a6352fae76b667fa002fa5d212
fracorvel <franpakopakito@gmail.com>
2018-01-15 04:03:57 +0100

    Renombrado de divide() y multiplica()

--- Archive/src/main/java/es/us/egc/App1.java
+++ Archive/src/main/java/es/us/egc/App1.java
@@ -16,12 +16,12 @@ public class App1
 		System.out.println(a + "/" + b + " = " + App1.divide(a,b));
 	}
 	
-	public static int divide(int a, int b){
+	public static int multiplica(int a, int b){
 		int result= a/b;
 		log.info("Returning "+a+"/"+b+"="+result);
 		return result;
 	}
-	public static int multiplica(int a, int b){
+	public static int divide(int a, int b){
 		int result= a*b;
 		log.info("Returning "+a+"*"+b+"="+result);
 		return result;
